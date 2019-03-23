538999e3e1185983827771a5221b06a97bb3e498
Tom MacLean <tom.j.maclean@gmail.com>
2019-03-23 16:45:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+  var newArr = [];
+  for(let i = 0; i < dwarves.length; i++){
+  newArr.push(`${i + 1}. ` + dwarves[i]);
+  }
+    return`${newArr.join(` `)} `;
 }
+  
 
 function summonCaptainPlanet(planeteerCalls){
+  var array = [];
+  for(let i = 0; i < planeteerCalls.length; i++){
+    var word = planeteerCalls[i].toUpperCase();
+    array.push(word + `!`);
+  }
+  return array;
 }
 
 function longPlaneteerCalls(words) {
+  for(let i = 0; i < words.length; i++){
+    if (words[i].length > 4){
+      return true;
+    }
+  }
+  return false ;
 }
 
 function findTheCheese (foods) {
+  var cheeses = ["cheddar", "gouda", "camembert"];
+  for(let i = 0; i < foods.length; i++){
+    if (cheeses.includes(foods[i])){
+      return `${foods[i]}`;
+    }
+  }
+  return "no cheese!" ;
 }
